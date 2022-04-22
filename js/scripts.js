284cfcc98b91e6569553aecea97e9ec6dd32d703
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-04-22 17:04:40 -0600

    Reconfigure Complete
    add a complete tested, and reconfigured intNumberToRobo();

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ function intNumberToRobo(userInput){
 
   for (let i=0; i <= userInput; i++) {
       arrayOfInts.push(i);
-      if (i>=0){ 
+      arrayOfInts.forEach(function(element){ 
         arrayOfInts.splice(i,1,i.toString().split(''));
 
         if (arrayOfInts[i].includes("1") === true && arrayOfInts[i].includes("2") === false && arrayOfInts[i].includes("3") === false) {
@@ -13,7 +13,7 @@ function intNumberToRobo(userInput){
         } else if (arrayOfInts[i].includes("3") === true) {
         arrayOfInts.splice(i,1,"Won't you be my neighbor?");
         }
-      }  
+      });  
   }
   return arrayOfInts.flat();
 };
