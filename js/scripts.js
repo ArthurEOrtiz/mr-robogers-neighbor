85b25c481bb3fb039a4d4d9db91e6190bc0f1b88
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-04-22 16:55:00 -0600

    Test Completed
    complete all testing requirements for intNumberToRobo(); and reconfigured.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
-//Buisness Logic
 function intNumberToRobo(userInput){
   let arrayOfInts = [];
 
-
-  for (let i=0; i <= userInput; i+= 1) {
+  for (let i=0; i <= userInput; i++) {
       arrayOfInts.push(i);
+      if (i>=0){ 
+        arrayOfInts.splice(i,1,i.toString().split(''));
 
-      if (i>=10){
-        arrayOfInts.push(i.toString().split(''));
-      }
-
-  };
-  arrayOfInts[1] = "Beep!";
-  arrayOfInts[2] = "Boop!";
-  arrayOfInts[3] = "Won't you be my neighbor?"
-  return arrayOfInts;
-
+        if (arrayOfInts[i].includes("1") === true && arrayOfInts[i].includes("2") === false && arrayOfInts[i].includes("3") === false) {
+        arrayOfInts.splice(i,1,"Beep!");
+        } else if (arrayOfInts[i].includes("2") === true && arrayOfInts[i].includes("3") === false) {
+        arrayOfInts.splice(i,1,"Boop!");
+        } else if (arrayOfInts[i].includes("3") === true) {
+        arrayOfInts.splice(i,1,"Won't you be my neighbor?");
+        }
+      }  
+  }
+  return arrayOfInts.flat();
 };
+
 
