6fcda4a4bd5e8d9ccbf865230d9ecb3f7fb70c14
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-04-22 12:24:38 -0600

    Test 4 Pass
    edit script page to meet all the requirements up to test 4 of intNumberToRobo();

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,14 @@
 function intNumberToRobo(userInput){
   let arrayOfInts = [];
 
+
   for (let i=0; i <= userInput; i+= 1) {
-    if (i>=10){
-      arrayOfInts.push(i.toString());
-    } else {
       arrayOfInts.push(i);
-    }
-  }
+  };
+  arrayOfInts[1] = "Beep!";
+  arrayOfInts[2] = "Boop!";
+  arrayOfInts[3] = "Won't you be my neighbor?"
   return arrayOfInts;
+
 };
 
