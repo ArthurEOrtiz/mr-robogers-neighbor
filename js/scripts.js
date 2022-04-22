8e178f46163738f88d680e1a563224577bbefa66
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-04-22 11:17:56 -0600

    First Test Complete
    Return the expected output of the first test of intNumberToRobot();

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 //Buisness Logic
-intNumberToRobo();
+function intNumberToRobo(userInput){
+  let arrayOfInts = [];
+
+  for (let i=0; i <= userInput; i+= 1) {
+    arrayOfInts.push(i);
+  }
+  return arrayOfInts;
+};
+
